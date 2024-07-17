aa1100469485eb7889a0b7cb0b0c7d80c61b83be
AngelMaga23 <angelmaga23@outlook.com>
2024-07-17 09:55:48 -0600

    Add Applications files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ApplicationsModule } from './applications/applications.module';
 
 @Module({
-  imports: [],
+  imports: [ApplicationsModule],
   controllers: [],
   providers: [],
 })

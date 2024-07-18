739769387b387844e3a60971f7c510cf63e064c8
AngelMaga23 <angelmaga23@outlook.com>
2024-07-18 17:03:38 -0600

    Add Entity Applications

--- src/applications/applications.module.ts
+++ src/applications/applications.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { ApplicationsService } from './applications.service';
 import { ApplicationsController } from './applications.controller';
+import { Application } from './entities/application.entity';
 
 @Module({
   controllers: [ApplicationsController],
   providers: [ApplicationsService],
+  imports:[
+    TypeOrmModule.forFeature([ Application ])
+  ]
 })
 export class ApplicationsModule {}
--- src/applications/entities/application.entity.ts
+++ src/applications/entities/application.entity.ts
@@ -1,1 +1,18 @@
-export class Application {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class Application {
+
+    @PrimaryGeneratedColumn('identity')
+    id: number;
+
+    @Column({type: 'varchar', length:255})
+    name: string;
+
+    @CreateDateColumn({ type: 'timestamp' })
+    created_at: Date;
+  
+    @UpdateDateColumn({ type: 'timestamp' })
+    updated_at: Date;
+
+}

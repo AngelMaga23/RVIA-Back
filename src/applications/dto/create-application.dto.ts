942380d2b7fb32a5b85fd531e6f4c18ba22b1257
AngelMaga23 <angelmaga23@outlook.com>
2024-07-18 17:04:07 -0600

    Update create-application.dto.ts

--- src/applications/dto/create-application.dto.ts
+++ src/applications/dto/create-application.dto.ts
@@ -1,1 +1,10 @@
-export class CreateApplicationDto {}
+import { IsString, MinLength } from "class-validator";
+
+export class CreateApplicationDto {
+
+    @IsString()
+    @MinLength(1)
+    name: string;
+
+
+}

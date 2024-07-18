80a00fa8cb99bb7c05344b97fc6185200f791452
AngelMaga23 <angelmaga23@outlook.com>
2024-07-18 17:07:07 -0600

    Add Entity and DTO sourcecode

--- src/sourcecode/dto/create-sourcecode.dto.ts
+++ src/sourcecode/dto/create-sourcecode.dto.ts
@@ -1,1 +1,13 @@
-export class CreateSourcecodeDto {}
+import { IsString, MinLength } from "class-validator";
+
+export class CreateSourcecodeDto {
+
+    @IsString()
+    @MinLength(1)
+    name: string;
+
+    @IsString()
+    @MinLength(1)
+    directory: string;
+
+}
--- src/sourcecode/entities/sourcecode.entity.ts
+++ src/sourcecode/entities/sourcecode.entity.ts
@@ -1,1 +1,19 @@
-export class Sourcecode {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class Sourcecode {
+    @PrimaryGeneratedColumn('identity')
+    id: number;
+
+    @Column({type: 'varchar', length:255})
+    name: string;
+
+    @Column({type: 'varchar', length:255})
+    directory: string;
+
+    @CreateDateColumn({ type: 'timestamp' })
+    created_at: Date;
+  
+    @UpdateDateColumn({ type: 'timestamp' })
+    updated_at: Date;
+}

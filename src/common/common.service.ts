27888078c3c507d91a3ac91e77755cd61c0f8776
AngelMaga23 <angelmaga23@outlook.com>
2024-07-30 12:27:50 -0600

    Update common.service.ts

--- src/common/common.service.ts
+++ src/common/common.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCommonDto } from './dto/create-common.dto';
 import { UpdateCommonDto } from './dto/update-common.dto';
+import { createCipheriv, createDecipheriv, randomBytes } from 'crypto';
 
 @Injectable()
 export class CommonService {
+
+  private readonly algorithm = 'aes-256-cbc';
+  private readonly key: Buffer;
+  private readonly iv: Buffer;
+
+  constructor() {
+    this.key = randomBytes(32); // La clave debe ser de 32 bytes para AES-256
+    this.iv = randomBytes(16); // El IV debe ser de 16 bytes
+  }
+
+
+  encrypt(text: string): string {
+    const cipher = createCipheriv(this.algorithm, this.key, this.iv);
+    let encrypted = cipher.update(text, 'utf8', 'hex');
+    encrypted += cipher.final('hex');
+    return `${this.iv.toString('hex')}:${encrypted}`;
+  }
+
+  decrypt(encryptedText: string): string {
+    const [ivHex, encrypted] = encryptedText.split(':');
+    const iv = Buffer.from(ivHex, 'hex');
+    const decipher = createDecipheriv(this.algorithm, this.key, iv);
+    let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+    return decrypted;
+  }
+
   create(createCommonDto: CreateCommonDto) {
     return 'This action adds a new common';
   }

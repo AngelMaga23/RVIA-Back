63fe28f048b23deb613c5f901a71883f4987de31
Brandon Jaimes <b1jaimes89@gmail.com>
2024-08-02 15:58:04 -0600

    `fix` file compressed 7z

--- src/applications/helper/ZIP/fileFilterzip.helper.ts
+++ src/applications/helper/ZIP/fileFilterzip.helper.ts
@@ -4,7 +4,14 @@ export const fileFilterZip = ( req: Express.Request, file: Express.Multer.File, 
 
     if (!file) return callback(new Error('File is empty'), false);
 
-    const fileExtension = file.mimetype.split('/')[1];
+    const name = file.originalname.split('.');
+    const ext = name[name.length - 1];
+
+    if(file && ext === '7z' && file.mimetype.includes('application/octet-stream')){
+        file.mimetype = 'application/x-7z-compressed';
+    }
+
+    const fileExtension = file.mimetype.split('/')[1];    
     const validExtensions = ['zip', 'x-zip-compressed', 'pdf', 'x-7z-compressed'];
    
     if (validExtensions.includes(fileExtension)) {
@@ -12,5 +19,4 @@ export const fileFilterZip = ( req: Express.Request, file: Express.Multer.File, 
     }
 
     callback(null, false);
-
 }

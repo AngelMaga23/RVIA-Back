d122c64be364e9fef65676d215a388b714a6fb37
AngelMaga23 <angelmaga23@outlook.com>
2024-08-05 17:58:40 -0600

    Update scans.controller.ts

--- src/scans/scans.controller.ts
+++ src/scans/scans.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { ScansService } from './scans.service';
 import { CreateScanDto } from './dto/create-scan.dto';
-import { UpdateScanDto } from './dto/update-scan.dto';
 
 @Controller('scans')
 export class ScansController {

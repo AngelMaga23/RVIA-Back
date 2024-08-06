0d5a6df30e00461bdeadf1e975ca1551d08ae1a7
AngelMaga23 <angelmaga23@outlook.com>
2024-08-06 16:39:19 -0600

    Update common.controller.ts

--- src/common/common.controller.ts
+++ src/common/common.controller.ts
@@ -1,8 +1,6 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 
 import { CommonService } from './common.service';
-import { CreateCommonDto } from './dto/create-common.dto';
-import { UpdateCommonDto } from './dto/update-common.dto';
 
 @Controller('common')
 export class CommonController {

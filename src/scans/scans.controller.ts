eeab21a52c8da715bd964cd8eaf5413fc2ca9dc1
AngelMaga23 <angelmaga23@outlook.com>
2024-08-06 16:34:42 -0600

    Update scans.controller.ts

--- src/scans/scans.controller.ts
+++ src/scans/scans.controller.ts
@@ -20,14 +20,5 @@ export class ScansController {
   findOne(@Param('id') id: string) {
     return this.scansService.findOne(+id);
   }
-
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateScanDto: UpdateScanDto) {
-  //   return this.scansService.update(+id, updateScanDto);
-  // }
-
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.scansService.remove(+id);
-  // }
+  
 }

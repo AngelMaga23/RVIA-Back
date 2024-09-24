f46151aba7822cee4f35c65c42dc4b94a667dedb
AngelMaga23 <angelmaga23@outlook.com>
2024-09-23 18:10:39 -0600

    Add Documentation Swagger applicationstatus

--- src/applicationstatus/applicationstatus.module.ts
+++ src/applicationstatus/applicationstatus.module.ts
@@ -5,13 +5,15 @@ import { ApplicationstatusService } from './applicationstatus.service';
 import { ApplicationstatusController } from './applicationstatus.controller';
 import { Applicationstatus } from './entities/applicationstatus.entity';
 import { CommonModule } from 'src/common/common.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [ApplicationstatusController],
   providers: [ApplicationstatusService],
   imports: [
     TypeOrmModule.forFeature([Applicationstatus]),
-    CommonModule
+    CommonModule,
+    AuthModule,
   ],
   exports:[
     ApplicationstatusService

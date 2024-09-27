1a4bbb95d21d7885c059a7a4ef0218195612a927
AngelMaga23 <angelmaga23@outlook.com>
2024-09-27 11:59:32 -0600

    Delete Endpoint Controller cost

--- src/cost/cost.controller.ts
+++ src/cost/cost.controller.ts
@@ -7,28 +7,28 @@ import { UpdateCostDto } from './dto/update-cost.dto';
 export class CostController {
   constructor(private readonly costService: CostService) {}
 
-  @Post()
-  create(@Body() createCostDto: CreateCostDto) {
-    return this.costService.create(createCostDto);
-  }
+  // @Post()
+  // create(@Body() createCostDto: CreateCostDto) {
+  //   return this.costService.create(createCostDto);
+  // }
 
-  @Get()
-  findAll() {
-    return this.costService.findAll();
-  }
+  // @Get()
+  // findAll() {
+  //   return this.costService.findAll();
+  // }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.costService.findOne(+id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.costService.findOne(+id);
+  // }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCostDto: UpdateCostDto) {
-    return this.costService.update(+id, updateCostDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateCostDto: UpdateCostDto) {
+  //   return this.costService.update(+id, updateCostDto);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.costService.remove(+id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.costService.remove(+id);
+  // }
 }

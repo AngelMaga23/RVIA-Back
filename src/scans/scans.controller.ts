7d8e2f17036dc975eae705735787d84d51ab5638
AngelMaga23 <angelmaga23@outlook.com>
2024-09-27 11:57:08 -0600

    Delete Endpoint Controller scans

--- src/scans/scans.controller.ts
+++ src/scans/scans.controller.ts
@@ -4,24 +4,24 @@ import { ScansService } from './scans.service';
 import { CreateScanDto } from './dto/create-scan.dto';
 
 
-@ApiTags('Escaneos')
+// @ApiTags('Escaneos')
 @Controller('scans')
 export class ScansController {
   constructor(private readonly scansService: ScansService) {}
 
-  @Post()
-  create(@Body() createScanDto: CreateScanDto) {
-    return this.scansService.create(createScanDto);
-  }
+  // @Post()
+  // create(@Body() createScanDto: CreateScanDto) {
+  //   return this.scansService.create(createScanDto);
+  // }
 
-  @Get()
-  findAll() {
-    return this.scansService.findAll();
-  }
+  // @Get()
+  // findAll() {
+  //   return this.scansService.findAll();
+  // }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.scansService.findOne(+id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.scansService.findOne(+id);
+  // }
   
 }
